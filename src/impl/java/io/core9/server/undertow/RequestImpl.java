b238f94b950e654bdb6586ad22aaff9c605bb3e1
Mark Wienk <mark.wienk@core9.io>
2014-07-15 16:18:52 +0200

    Added form multipart

--- src/impl/java/io/core9/server/undertow/RequestImpl.java
+++ src/impl/java/io/core9/server/undertow/RequestImpl.java
@@ -2,23 +2,25 @@ package io.core9.server.undertow;
 
 import io.core9.plugin.server.Cookie;
 import io.core9.plugin.server.VirtualHost;
+import io.core9.plugin.server.request.FileUpload;
 import io.core9.plugin.server.request.Method;
 import io.core9.plugin.server.request.Request;
 import io.core9.plugin.server.request.Response;
 import io.undertow.server.HttpServerExchange;
+import io.undertow.server.handlers.form.FormData;
+import io.undertow.server.handlers.form.FormData.FormValue;
+import io.undertow.server.handlers.form.FormDataParser;
+import io.undertow.server.handlers.form.FormParserFactory;
 import io.undertow.util.HeaderValues;
+import io.undertow.util.Headers;
 
 import java.io.InputStreamReader;
-import java.io.UnsupportedEncodingException;
-import java.net.URLDecoder;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Deque;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
-import java.util.Set;
 
 import net.minidev.json.JSONArray;
 import net.minidev.json.JSONObject;
@@ -30,45 +32,100 @@ import rx.schedulers.Schedulers;
 import com.google.common.io.CharStreams;
 
 public class RequestImpl implements Request {
-	
+
+	private static final String APPLICATION_JSON = "application/json";
+
 	private HttpServerExchange exchange;
 	private final VirtualHost vhost;
-	private Map<String,Object> params;
-	private Map<String,Object> context;
+	private Map<String, Object> params;
+	private Map<String, Object> context;
 	private ResponseImpl response;
-	
+
 	private String strBody;
-	
-	private Observable<String> body = Observable.create( (OnSubscribe<String>) subscriber -> {
-		if(strBody != null) {
+	private JSONObject mapBody;
+	private JSONArray listBody;
+
+	/**
+	 * TODO: Tidy up (refactor)
+	 */
+	private Observable<String> body = Observable.create((OnSubscribe<String>) subscriber -> {
+		if (strBody != null) {
 			subscriber.onNext(strBody);
 		} else {
-			if(exchange.isInIoThread()) {
+			if (exchange.isInIoThread()) {
 				subscriber.onError(new IllegalStateException("Blocking operation is in IO thread, not allowed!"));
 			} else {
 				exchange.startBlocking();
 			}
 			try {
-				this.strBody = CharStreams.toString(new InputStreamReader(exchange.getInputStream()));
+				String mimeType = exchange.getRequestHeaders().getFirst(Headers.CONTENT_TYPE);
+				if (mimeType != null && mimeType.startsWith(APPLICATION_JSON)) {
+					Object tmpBody = JSONValue.parse(exchange.getInputStream());
+					if (tmpBody instanceof JSONObject) {
+						mapBody = (JSONObject) tmpBody;
+						strBody = mapBody.toJSONString();
+					} else if (tmpBody instanceof JSONArray) {
+						listBody = (JSONArray) tmpBody;
+						strBody = listBody.toJSONString();
+					} else {
+						throw new IllegalStateException("A non JSON value was emitted to a JSON parser");
+					}
+				} else {
+					FormDataParser parser = FormParserFactory.builder().build().createParser(exchange);
+					if (parser != null) {
+						FormData data = parser.parseBlocking();
+						mapBody = new JSONObject();
+						data.forEach((key) -> {
+							Deque<FormValue> values = data.get(key);
+							List<FileUpload> files = new ArrayList<FileUpload>();
+							for (FormValue value : values) {
+								if (value.isFile()) {
+									HeaderValues content = value.getHeaders().get(Headers.CONTENT_DISPOSITION);
+									String folder = "/";
+									for(String item : content) {
+										System.out.println(item);
+										int idx = item.indexOf("name=\"");
+										if(idx != -1) {
+											folder = item.substring(idx + 6);
+											folder = folder.substring(0, item.indexOf(";") -2);
+										}
+									};
+									files.add(new FileUploadImpl(folder + value.getFileName(), 
+											value.getHeaders().getFirst(Headers.CONTENT_TYPE_STRING), 
+											value.getFile().getPath()));
+								}
+							}
+							if (files.isEmpty()) {
+								mapBody.put(key, data.get(key));
+							} else {
+								context.put("files", files);
+							}
+						});
+						strBody = mapBody.toJSONString();
+					} else {
+						strBody = CharStreams.toString(new InputStreamReader(exchange.getInputStream()));
+					}
+				}
 				subscriber.onNext(strBody);
 			} catch (Exception e) {
 				subscriber.onError(e);
 			}
-		}
 		subscriber.onCompleted();
+		}
 	}).subscribeOn(Schedulers.io());
-	
+
 	public RequestImpl(VirtualHost vhost, HttpServerExchange exchange) {
 		this.exchange = exchange;
 		this.vhost = vhost;
-		if(exchange.getQueryParameters().size() > 0) {
-			this.params = new HashMap<String,Object>();
-			for(Entry<String, Deque<String>> entry : exchange.getQueryParameters().entrySet()) {
-				this.params.put(entry.getKey(), entry.getValue().getLast()); // FIXME: Handle all query params (is multiple possible?)
+		if (exchange.getQueryParameters().size() > 0) {
+			this.params = new HashMap<String, Object>();
+			for (Entry<String, Deque<String>> entry : exchange.getQueryParameters().entrySet()) {
+				// FIXME: Handle all query params (is multiple possible?)
+				this.params.put(entry.getKey(), entry.getValue().getLast());
 			}
 		}
 	}
-	
+
 	public HttpServerExchange getExchange() {
 		return exchange;
 	}
@@ -80,7 +137,7 @@ public class RequestImpl implements Request {
 
 	@Override
 	public Map<String, Object> getParams() {
-		if(params != null) {
+		if (params != null) {
 			return params;
 		}
 		return params = new HashMap<>();
@@ -88,7 +145,7 @@ public class RequestImpl implements Request {
 
 	@Override
 	public Response getResponse() {
-		if(response != null) {
+		if (response != null) {
 			return response;
 		}
 		return response = new ResponseImpl(this.vhost, this.exchange);
@@ -106,29 +163,15 @@ public class RequestImpl implements Request {
 
 	@Override
 	public Observable<Map<String, Object>> getBodyAsMap() {
-		return Observable.create((OnSubscribe<Map<String,Object>>) subscriber -> {
-			this.getBody().subscribe((value) -> {
-				try {
-					HeaderValues contentTypes = exchange.getRequestHeaders().get("Content-Type");
-					if(contentTypes == null) {
-						subscriber.onError(new UnsupportedEncodingException("No content-type header set"));
-					}
-					boolean json = false;
-					for(String contentType : contentTypes) {
-						if(contentType.contains("application/json")) {
-							json = true;
-						}
-					}
-					if(json) {
-						subscriber.onNext(((JSONObject) JSONValue.parse(value)));
-					} else {
-						subscriber.onNext(splitQuery(value));
-					}
-					subscriber.onCompleted();
-				} catch (Exception e) {
-					subscriber.onError(e);
-				}
-			});
+		return Observable.create((OnSubscribe<Map<String, Object>>) subscriber -> {
+			if(mapBody != null) {
+				subscriber.onNext(mapBody);
+				subscriber.onCompleted();
+			} else {
+				this.body.toBlocking().last();
+				subscriber.onNext(mapBody);
+				subscriber.onCompleted();
+			}
 		}).subscribeOn(Schedulers.io());
 	}
 
@@ -136,18 +179,15 @@ public class RequestImpl implements Request {
 	public Observable<List<Object>> getBodyAsList() {
 		return Observable.create((OnSubscribe<List<Object>>) subscriber -> {
 			this.getBody().subscribe((value) -> {
-				try {
-					subscriber.onNext((JSONArray) JSONValue.parse(value));
-				} catch (Exception e) {
-					subscriber.onError(e);
-				}
+				subscriber.onNext(listBody);
+				subscriber.onCompleted();
 			});
 		}).subscribeOn(Schedulers.io());
 	}
 
 	@Override
 	public Map<String, Object> getContext() {
-		if(context != null) {
+		if (context != null) {
 			return context;
 		}
 		return context = new HashMap<>();
@@ -178,7 +218,7 @@ public class RequestImpl implements Request {
 
 	@Override
 	public Cookie getCookie(String name) {
-		if(!exchange.getRequestCookies().containsKey(name)) {
+		if (!exchange.getRequestCookies().containsKey(name)) {
 			return null;
 		}
 		return new CookieImpl(exchange.getRequestCookies().get(name));
@@ -196,7 +236,7 @@ public class RequestImpl implements Request {
 
 	@Override
 	public void setCookies(List<Cookie> cookies) {
-		for(Cookie cookie : cookies) {
+		for (Cookie cookie : cookies) {
 			exchange.setResponseCookie(((CookieImpl) cookie).getServerCookie());
 		}
 	}
@@ -204,40 +244,6 @@ public class RequestImpl implements Request {
 	@Override
 	public String getHostname() {
 		return exchange.getHostAndPort();
-	}
-	
-	@SuppressWarnings("unchecked")
-    private static Map<String, Object> splitQuery(String query) {
-		if(query == null || query.length() == 0) return new HashMap<String, Object>();
-		String[] pairs = query.split("&");
-		Map<String, Object> query_pairs = new HashMap<String, Object>();
-		for (String pair : pairs) {
-			int idx = pair.indexOf("=");
-			try {
-				String key = URLDecoder.decode(pair.substring(0, idx), "UTF-8");
-				String value = URLDecoder.decode(pair.substring(idx + 1), "UTF-8");
-				Set<String> queryKeys = query_pairs.keySet();
-				
-				if(!queryKeys.isEmpty() && queryKeys.contains(key)){
-					ArrayList<String> paramList = new ArrayList<>();
-					Object list = query_pairs.get(key);
-					if(list instanceof String){
-						paramList.add(value);
-					}else if(list instanceof ArrayList){
-						paramList.addAll((Collection<? extends String>) query_pairs.get(key));
-						paramList.add(value);
-					} 
-					query_pairs.put(key, paramList);
-				}else{
-					query_pairs.put(key,value);
-				}
-				
-				
-			} catch (UnsupportedEncodingException e) {
-				return new HashMap<>();
-			}
-		}
-		return query_pairs;
 	}
 
 }
